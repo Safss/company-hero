5c0855ef57a7cc815dbbd5cb539f88dcd8629c80
marcoswedoc <marcosp@wedoc.com.br>
2024-06-11 18:41:37 -0300

    added spotify parametrs

--- config/globals.py
+++ config/globals.py
@@ -3,3 +3,6 @@ import os
 
 URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather"
 API_KEY_WEATHER = str(os.getenv("weatherApikey", ""))
+SPOTIFY_URL_AUTH = "https://accounts.spotify.com/api/token"
+SPOTIFY_CLIENT_ID = str(os.getenv("spotifyClientId", ""))
+SPOTIFY_CLIENT_SECRET = str(os.getenv("spotifyClientSecret", ""))
