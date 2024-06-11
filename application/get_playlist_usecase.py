aabd3f3c3dee9496795534f4b6f94fba5d1c7c86
marcoswedoc <marcosp@wedoc.com.br>
2024-06-11 13:06:56 -0300

    added repository call to get playlist in usecase

--- application/get_playlist_usecase.py
+++ application/get_playlist_usecase.py
@@ -1,12 +1,14 @@
 from typing import Any, Dict, Optional
 from domain.entity.playlist import PlayList
 from domain.errors.exceptions import MissingCityException
+from domain.repository.i_playlist_repository import IPlayListRepository
 
 from domain.repository.i_weather_repository import IWeatherRepository
 
 class GetPlaylistUseCase:
-    def __init__(self, weather_repository: IWeatherRepository):
+    def __init__(self, weather_repository: IWeatherRepository, playlist_repository: IPlayListRepository):
         self.weather_repository = weather_repository
+        self.playlist_repository = playlist_repository
 
     def execute(self, event: Dict[str, Any]) -> Dict[str, Any]:
         city = self.get_city_from_event(event)
@@ -15,11 +17,15 @@ class GetPlaylistUseCase:
         
         playlist = PlayList(city=city)
         playlist.temperature = self.get_city_temperature(city=city)
+        playlist.playlist = self.get_playlist(playlist.musical_genre)
 
         return playlist.generate_playlist_response()
     
     def get_city_temperature(self, city: str) -> int:
-        return self.weather_repository.get_weather_termperature(city=city)
+        return self.weather_repository.get_weather_termperature(city)
+    
+    def get_playlist(self, musical_genre: str) -> Dict[str, Any]:
+        return self.playlist_repository.get_playlist(musical_genre)
     
     def get_city_from_event(self, event: Dict[str, Any]) -> Optional[str]:
         queryString = event.get("queryStringParameters", {})
